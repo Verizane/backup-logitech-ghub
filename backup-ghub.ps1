4b5a8cb33a8918c30466c6c07e759b7feaf578d5
Verizane <Roman.Koslowski@gmail.com>
2023-07-13 00:50:35 +0200

    Update backup-ghub.ps1

--- backup-ghub.ps1
+++ backup-ghub.ps1
@@ -1,16 +1,28 @@
 # Settings
 $file_name = "ghub-$(Get-Date -Format 'dd.MM.yyyy.hh.mm').backup.zip";
-$ghub_settings_path = "$($env:LOCALAPPDATA)\LGHUB";
-$output_dir = "$($env:HOMEDRIVE)$($env:HOMEPATH)\ghub-backup\";
+$ghub_local_settings_path = "$($env:LOCALAPPDATA)\LGHUB";
+$ghub_roaming_settings_path = "$($env:APPDATA)\LGHUB";
+$output_dir = "$($env:HOMEDRIVE)$($env:HOMEPATH)\Documents\ghub-backup\";
+$output_dir_tmp = "$($output_dir)\tmp\";
+$output_dir_tmp_local = "$($output_dir_tmp)\local\";
+$output_dir_tmp_roaming = "$($output_dir_tmp)\roaming\";
 $output_path = "$output_dir\$file_name";
 
 # Change this to prefered time
 $time_limit = (Get-Date).AddDays(-90);
 
 # Creates directory
-New-Item -ItemType Directory -Force -Path "$output_dir";
+New-Item -ItemType Directory -Force -Path "$output_dir_tmp_local";
 
-Compress-Archive -Path "$ghub_settings_path" -DestinationPath "$output_path";
+New-Item -ItemType Directory -Force -Path "$output_dir_tmp_roaming";
+
+Copy-Item -Recurse -Path "$ghub_local_settings_path" -Destination "$output_dir_tmp_local";
+
+Copy-Item -Recurse -Path "$ghub_roaming_settings_path" -Destination "$output_dir_tmp_roaming";
+
+Compress-Archive -Path "$output_dir_tmp_local", "$output_dir_tmp_roaming" -DestinationPath "$output_path";
+
+Get-Item $output_dir_tmp | Remove-Item -Force -Recurse;
 
 # Source: https://stackoverflow.com/a/19326146/2446102
 # Remove files older than $time_limit
